2d2e0ae3c42b7f048a213ced2b4008b88304a1c7
Hitendra Singh <hitendra@cialfo.com.sg>
2016-03-29 13:31:41 +0530

    Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :models
+  devise_for :users
+  
+ 
   get 'welcome/index'
 
   root 'welcome#index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160328183953) do
+ActiveRecord::Schema.define(version: 20160328184356) do
 
   create_table "docs", force: :cascade do |t|
     t.string   "title"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20160328183953) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "models", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -31,12 +31,11 @@ ActiveRecord::Schema.define(version: 20160328183953) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "User"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "models", ["email"], name: "index_models_on_email", unique: true
-  add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
